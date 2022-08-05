0f7b15e91ec3e11164b2dcf0c856128941bb58f7
Polina-Novik <polinochka_novik_2002@mail.ru>
2022-08-05 20:45:31 +0300

    Добавлены примеры операторов ветвления, повторения, переходов

---
+++ src/ChetnoePoriadock.java
@@ -0,0 +1,15 @@
+public class ChetnoePoriadock {
+    public static void main(String[] args) {
+        //for(int i=0 или 1 типа нач знач; условие? i<10 INT>0 при этом идет и невыполнвыход; ччо хотим i++
+        for (int i = 0; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+
+        }
+        for (int i = 10; i >= 0; i--) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }}
